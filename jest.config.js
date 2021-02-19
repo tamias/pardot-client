c25deec33bd1a7f931ce4db30e88653aeaa9a292
Ronald J Kimball <rjk@tamias.net>
2021-02-19 14:30:44 -0600

    Report test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
+/* eslint-disable sort-keys */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      function: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
